5b718495654e3a84ccce2b8dfdf5c41607079f5d
Ben Everard <hello@beneverard.co.uk>
2015-02-28 11:37:55 +0000

    Tidied up plugin loader

--- acf-data-selector.php
+++ acf-data-selector.php
@@ -21,23 +21,23 @@ load_plugin_textdomain( 'acf-data-selector-field', false, dirname( plugin_basena
 
 // 2. Include field type for ACF5
 // $version = 5 and can be ignored until ACF6 exists
-function include_field_types_data_selector-field( $version ) {
+function include_field_types_data_selector_field( $version ) {
 	
 	include_once('acf-data-selector-field-v5.php');
 	
 }
 
-add_action('acf/include_field_types', 'include_field_types_data_selector-field');	
+add_action('acf/include_field_types', 'include_field_types_data_selector_field');	
 
 
 
 
 // 3. Include field type for ACF4
-function register_fields_data-selector_field() {
+function register_fields_data_selector_field() {
 	
 	include_once('acf-data-selector-field-v4.php');
 	
 }
 
-add_action('acf/register_fields', 'register_fields_data-selector_field');	
+add_action('acf/register_fields', 'register_fields_data_selector_field');	
 
